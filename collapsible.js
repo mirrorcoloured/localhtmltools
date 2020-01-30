a6d8628e5b6f970e0480a8be0d3a05e71668517f
Sky <mirrorcoloured@hotmail.com>
2020-01-30 14:05:16 -0700

    collapsible updates on all changes

--- collapsible.js
+++ collapsible.js
@@ -1,19 +1,37 @@
-function addCollapsibleEvents() {
-    var coll = document.getElementsByClassName("collapsible_button");
-    var i;
-
-    for (i = 0; i < coll.length; i++) {
-        coll[i].addEventListener("click", function() {
-                this.classList.toggle("collapsed");
-                var content = this.nextElementSibling;
-                if (content.style.maxHeight){
-                    content.style.maxHeight = null;
-                } else {
-                    content.style.maxHeight = content.scrollHeight + "px";
-                }
-                content.classList.toggle("collapsed");
-        });
-        coll[i].click(); // Start expanded
+function updateCollapsible(collapsible) {
+    let [button, content] = getButtonAndContent(collapsible);
+    if (content.classList.contains("expanded")) {
+        content.style.maxHeight = content.scrollHeight + "px";
+    } else {
+        content.style.maxHeight = 0;
     }
 }
+
+function getButtonAndContent(collapsible) {
+    const button = collapsible.getElementsByClassName("collapsible_button")[0];
+    const content = collapsible.getElementsByClassName("collapsible_content")[0];
+    return [button, content];
+}
+
+function addCollapsibleEvents() {
+    for (let collapsible of document.getElementsByClassName("collapsible")) {
+        let [button, content] = getButtonAndContent(collapsible);
+
+        button.addEventListener("click", function() {
+            button.classList.toggle("active");
+            content.classList.toggle("expanded");
+        });
+
+        const observer = new MutationObserver(function() {
+            updateCollapsible(collapsible);
+        });
+        observer.observe(collapsible, { attributes: true, childList: true, subtree: true });
+
+        // Start expanded
+        button.classList.toggle("active");
+        content.classList.toggle("expanded");
+        updateCollapsible(collapsible)
+    }
+}
+
 addCollapsibleEvents();
